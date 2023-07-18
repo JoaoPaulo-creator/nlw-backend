19da7a42507022e68e667954da2e25e266580c47
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-17 21:05:40 -0300

    refactor: Add any type until a correct is provided

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,24 @@
 import * as dotenv from 'dotenv'
+import app from '../src/server'
 
 // Require the framework
 import Fastify from 'fastify'
+
 dotenv.config()
 
+const teste: any = app
+
 // Instantiate Fastify with some config
-const app = Fastify({
+const server = Fastify({
   logger: false,
 })
 
 // Register your application as a normal plugin.
-app.register(import('../src/server'), {
+server.register(teste, {
   prefix: '/',
 })
 
-export default async (req, res) => {
-  await app.ready()
-  app.server.emit('request', req, res)
+export default async (req: any, res: any) => {
+  await server.ready()
+  server.server.emit('request', req, res)
 }
